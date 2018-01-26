82caaf1e3947fa947a324e2b17e79149a9784632
Yiming Kang <kang@yimings-mbp.wusm-wifi.wucon.wustl.edu>
2018-01-26 14:34:09 -0600

    initialized dirs, added make job-scripts tool

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Human RNA-seq pipeline
 
 ### Package installation
-Follow instructions from https://github.com/griffithlab/rnaseq_tutorial/wiki/Installation
-*** Use OSX version instead of Linux if needed
+Follow instructions from https://github.com/griffithlab/rnaseq_tutorial/wiki/Installation.
+*** Use OSX version instead of Linux, if working on a Mac.
 
 ### Configure tools
+Required for each run. This configures the paths for executable tools.
 ```
 export PATH=tools/hisat2-2.0.0-beta/:$PATH
 export PATH=tools/samtools-1.3.1/:$PATH
@@ -12,11 +13,19 @@ export PATH=tools/stringtie-1.3.3b.OSX_x86_64/:$PATH
 ```
 
 ### Build genome index
+This builds the genome index by `hisat2-build` tool.
 ```
 ./tools/hisat2-2.0.0-beta/hisat2-build GRCh38/chr22_ERCC92.fa GRCh38/hisat2_index/chr22_ERCC92
 ```
 
 ### Make job script to run alignment
+This automatically makes the job scripts to process all samples based on the sample summary file.
 ```
 python tools/build_job_script.py -s metadata/sample_summary.txt -i GRCh38/hisat2_index/chr22_ERCC92 > job_scripts/alignment.makefile
+```
+
+### Run jobs
+It uses `make` mechanism to process samples with parallelization if applicable. Define number of jobs running in parallel with `-j`. The configuration of tool paths may require adjustment if working with task management system, e.g. dispatching distributed jobs.
+```
+make -j 2 -f job_scripts/alignment.makefile
 ```
---
+++ tools/build_job_script.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+import sys
+import argparse
+import pandas as pd
+
+
+def parse_args(argv):
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-s', '--samples', 
+						help='Sample summary metadata file.')
+	parser.add_argument('-i', '--genome_index', 
+						help='Prefix of genome index files built for the aligner, e.g. hisat2')
+	parser.add_argument('-r', '--reference_gtf', 
+						help='Annotation reference file in GTF/GFF3 format.')
+	parser.add_argument('-g', '--gene_list',
+						help='Gene list.')
+	return parser.parse_args(argv[1:])
+
+
+def build_alignment(samples, genome_index):
+	## define alignment dictionary
+	align_dict = {}
+	for i,row in samples.iterrows():
+		## define variables
+		sample = row['SAMPLE']
+		seqs = [x.strip('" ') for x in row['SEQUENCE'].split(',')]
+		paired = True if len(seqs) == 2 else False
+		target_dir = 'alignment/hisat2/'+ sample
+		bam_file = target_dir +'/aligned_reads.bam'
+		sorted_bam_file = target_dir +'/aligned_reads_sorted.bam'
+		## create cleaned directory
+		recipe = 'rm -rf '+ target_dir +'; mkdir '+ target_dir +'; '
+		## align reads
+		## TODO: add arugment for trim5
+		if paired:
+			recipe += 'hisat2 -p 2 --rg-id='+ sample + ' -x '+ genome_index +' --dta --rna-strandness RF -1 '+ seqs[0] +' -2 '+ seqs[1] +' -S '+ bam_file +'; '
+		else:
+			recipe += 'hisat2 -p 2 --rg-id='+ sample + ' -x '+ genome_index +' --dta --rna-strandness RF -U '+ seqs[0] +' -S '+ bam_file +'; '
+		## sort bam file
+		recipe += 'samtools sort -t /tmp/'+ sample +'.sort_bam -o '+ bam_file +' '+ sorted_bam_file +'; '
+		recipe += 'rm ' + bam_file
+		## set alignment dictionary
+		align_dict[sorted_bam_file] = {'prereq': ' '.join(seqs), 
+									'recipe': recipe}
+	return align_dict
+
+
+def build_expression_quantification(samples, reference_gtf, gene_list):
+	expr_dict = {}
+	for i,row in samples.iterrows():
+		sample_id = row['GENOTYPE'] +'-'+ row['SAMPLE']
+		prereq = 'alignment/hisat2/'+ sample_id +'/aligned_reads_sorted.bam'
+		target_dir = 'expression/stringtie/'+ sample_id
+		target_file = 'expression/stringtie_fpkm/'+ sample_id +'.expr'
+		recipe = 'rm -rf '+ target_dir +'; mkdir '+ target_dir +'; '
+		recipe += 'stringtie '+ prereq +' -G '+ reference_gtf +' -e -o '+ target_dir +'/stringtie_out.gtf -A '+ target_dir +'/gene_abundances.tab; '
+		recipe += 'python tools/stringtie2fpkm.py '+ target_dir +'/gene_abundances.tab '+ gene_list +' > '+ target_file
+		expr_dict[target_file] = {'prereq': prereq, 'recipe': recipe}
+	return expr_dict
+
+
+def main(argv):
+	parsed = parse_args(argv)
+	samples = pd.read_csv(parsed.samples, delimiter='\t', dtype=str)
+	
+	align_dict = build_alignment(samples, parsed.genome_index)
+	for target in sorted(align_dict.keys()):
+		sys.stdout.write('%s: %s\n\t%s\n' % (target, align_dict[target]['prereq'], align_dict[target]['recipe']))
+	sys.stdout.write('HISAT2_ALIGNMENTS = %s\n' % ' '.join(sorted(align_dict.keys())))
+
+	# expr_dict = build_expression_quantification(samples, parsed.reference_gtf, parsed.gene_list)
+	# for target in sorted(expr_dict.keys()):
+	# 	sys.stdout.write('%s: %s\n\t%s\n' % (target, expr_dict[target]['prereq'], expr_dict[target]['recipe']))
+	# sys.stdout.write('STRINGTIE_EXPRESSIONS = %s\n' % ' '.join(sorted(expr_dict.keys())))
+
+	sys.stdout.write('all: $(HISAT2_ALIGNMENTS)')
+	# sys.stdout.write('all: $(HISAT2_ALIGNMENTS) $(STRINGTIE_EXPRESSIONS)')
+
+if __name__ == '__main__':
+	main(sys.argv)
